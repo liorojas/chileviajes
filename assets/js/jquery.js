73213384ac6b64ae67afe40f05e17f1ff876912f
Lio Fumacarne <liofumacarne@MacBook-Pro-de-Lio.local>
2023-04-26 19:02:43 -0400

    final commit

---
+++ assets/js/jquery.js
@@ -0,0 +1,12 @@
+// toggle en quienes somos
+$(document).ready(function(){
+    $("#desaparecer").click(function(){
+        $(".desaparecer1").toggle();
+        });
+        $("#desaparecer2").click(function(){
+            $(".desaparecer2").toggle();
+            });
+            $("#desaparecer3").click(function(){
+                $(".desaparecer3").toggle();
+                });
+    });
